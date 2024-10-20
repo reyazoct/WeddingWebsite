91724783537a737ee877992f07e076c4f48800e5
Reyaz Ahmad <reyazoct@gmail.com>
2024-10-20 08:33:20 +0530

    Add files

---
+++ src/components/Layouts/GuestLayout.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
+import useDB from "@/hooks/useDB";
+import Menu from "@/components/Menu";
+import SmoothScroll from "./SmoothScroll";
+import { BrowserView, MobileView } from "react-device-detect";
+
+/**
+ * Guest layout
+ *
+ * @param {React.ReactNode} options.children
+ * @param {string} options.title
+ *
+ * @return {React.ReactElement}
+ */
+const GuestLayout = ({ children, ...rest }) => {
+  const app = useDB((db) => db.application);
+
+  /**
+   * Update title pada document.
+   */
+  React.useEffect(() => {
+    document.title = app.name;
+  }, [app]);
+
+  /**
+   * Konten
+   */
+  const content = (
+    <Box component="main" id="mainContent" {...rest}>
+      {children}
+    </Box>
+  );
+
+  /**
+   * Render komoponen.
+   */
+  return (
+    <>
+      <Menu />
+
+      <BrowserView>
+        <SmoothScroll>{content}</SmoothScroll>
+      </BrowserView>
+
+      <MobileView>{content}</MobileView>
+    </>
+  );
+};
+
+/**
+ * Prop types
+ */
+GuestLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+/**
+ * Default props
+ */
+GuestLayout.defaultProps = {};
+
+export default GuestLayout;
